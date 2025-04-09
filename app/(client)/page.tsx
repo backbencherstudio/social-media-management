634d8c35a84fe55b3f385d68a0e223d4b1656540
saidul147 <sidul.islam.146@gmail.com>
2025-04-09 15:15:57 +0600

    hero added

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Navbar } from './_components/navbar'
+import Hero from './_components/landing-page/hero'
 
 export default function HomePage() {
   return (
     <div>
         <Navbar />
+        <Hero/>
+
+
+
+
+
+        
     </div>
   )
 }
