22696517ee095c93a4aa4e9e52f7c37b40a9a8d1
tushar <bbsfullstack@gmail.com>
2025-04-10 13:14:59 +0600

    added

---
+++ app/(client)/pricing-and-package/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from './_components/banner'
+import Pricing from './_components/pricing'
+
+export default function PricingAndPackage() {
+  return (
+    <div>
+        <Banner/>
+        <Pricing/>
+    </div>
+  )
+}
