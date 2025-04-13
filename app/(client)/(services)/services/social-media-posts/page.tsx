159030b9c93b7e0aa77dbac704349f7c64eb7e7f
tushar <tushar789imran@gmail.com>
2025-04-13 12:58:10 +0600

    added banner

--- app/(client)/(services)/services/social-media-posts/page.tsx
+++ app/(client)/(services)/services/social-media-posts/page.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import ReUsableBanner from "@/app/(client)/_components/re-usable-banner";
+import React from "react";
+import bannerImg from "@/public/img/social-media-posts/banner.png";
+import Heading from "@/app/(client)/_components/heading-text";
+import ParagraphText from "@/app/(client)/_components/paragraph-text";
 
 export default function SocialMediaPost() {
   return (
-    <div>SocialMediaPost</div>
-  )
+    <div>
+      <ReUsableBanner
+        heading={
+          <Heading
+            text="Professional Post Design Services Starting at Just $149/mo to $99/mo"
+            className="text-[42px]"
+          />
+        }
+        description={
+          <ParagraphText
+            paraText="Take your email marketing to the next level with stunning emails that convert. Made by our team of copywriters, designers, and email experts."
+            className="text-base"
+          />
+        }
+        image={bannerImg}
+      />
+    </div>
+  );
 }
