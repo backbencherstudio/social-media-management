a405766bf28a24eb4ddf4470de034f0e1c07d2ba
saidul147 <sidul.islam.146@gmail.com>
2025-04-18 11:41:54 +0600

    instagram growth done

--- app/(client)/(services)/services/instagram-growth/page.tsx
+++ app/(client)/(services)/services/instagram-growth/page.tsx
@@ -2,6 +2,10 @@ import ReuseableHero from '@/app/(client)/_components/reuseable-hero';
 import React from 'react';
 import img from "@/public/instaGrowth.png"
 import InstaGrowth from './_components/insta-growth';
+import ModernInstaMarketing from './_components/modern-insta-marketing';
+import InstastoryExample from '@/app/(client)/examples/_components/instastory-example';
+import PostExample from '@/app/(client)/examples/_components/post-content';
+import FrequentlyAsked from '@/app/(client)/_components/landing-page/frequently-asked';
 
 const InstaGrowthPage = () => {
     return (
@@ -13,6 +17,10 @@ const InstaGrowthPage = () => {
             ]} img={img} />
 
             <InstaGrowth />
+            <ModernInstaMarketing />
+            <PostExample header="Examples of Instagram post" para = "Get your design & marketing work done without the hassle of unreliable freelancers, costly agencies. Pay a fixed, monthly, and predictable rate, with no contracts or surprises." />
+            <InstastoryExample />
+            <FrequentlyAsked />
         </div>
     );
 };
