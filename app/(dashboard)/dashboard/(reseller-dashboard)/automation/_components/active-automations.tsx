6b171939e45c858d59d5d97f9e4607ce7e6b8813
mahmudul-hasan01 <mahamudul2018@gmail.com>
2025-05-16 14:50:42 +0600

    ädd something new

--- app/(dashboard)/dashboard/(reseller-dashboard)/automation/_components/active-automations.tsx
+++ app/(dashboard)/dashboard/(reseller-dashboard)/automation/_components/active-automations.tsx
@@ -56,7 +56,7 @@ export default function ActiveAutomations() {
                 </p>
                 <label className="relative inline-flex items-center cursor-pointer">
                   <input type="checkbox" className="sr-only peer" />
-                  <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-green-600 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"></div>
+                  <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-green-600 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600">gg</div>
                 </label>
               </div>
             </div>
